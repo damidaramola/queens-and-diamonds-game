f4e4eca935542251089615ba3ea90af4a33ac265
damidaramola <damidaramola7@gmail.com>
2022-12-31 22:01:03 +0000

    add back image of the cards

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,19 +1,25 @@
 //file paths of 4 card images used in the game
-const cardObjectDefined = [
-    {
-      id: 1,
-      imagePath: "assets/images/king-of-clubs.PNG",
+const cardObjectDefined = [{
+        id: 1,
+        imagePath: "assets/images/king-of-clubs.PNG",
     },
     {
-      id: 2,
-      imagePath: "assets/images/Ace-of-Spades.PNG",
+        id: 2,
+        imagePath: "assets/images/Ace-of-Spades.PNG",
     },
     {
-      id: 3,
-      imagePath: "assets/images/Jack-of-hearts.PNG",
+        id: 3,
+        imagePath: "assets/images/Jack-of-hearts.PNG",
     },
     {
-      id: 4,
-      imagePath: "assets/images/Queen-ofDiamonds.PNG",
+        id: 4,
+        imagePath: "assets/images/Queen-ofDiamonds.PNG",
     }
-  ];
+];
+
+const queenId = 4;
+var form = document.getElementById("form");
+//Back image of cards
+const cardBackImgPath = "assets/images/card-back-Blue.png";
+
+let cards = [];
