aadceac4a7cef2f7f1f930f29494f15d967cb12c
damidaramola <damidaramola7@gmail.com>
2022-12-31 21:59:42 +0000

    add 4 images of cards to js

---
+++ assets/js/style.js
@@ -0,0 +1,19 @@
+//file paths of 4 card images used in the game
+const cardObjectDefined = [
+    {
+      id: 1,
+      imagePath: "assets/images/king-of-clubs.PNG",
+    },
+    {
+      id: 2,
+      imagePath: "assets/images/Ace-of-Spades.PNG",
+    },
+    {
+      id: 3,
+      imagePath: "assets/images/Jack-of-hearts.PNG",
+    },
+    {
+      id: 4,
+      imagePath: "assets/images/Queen-ofDiamonds.PNG",
+    }
+  ];
